144d9d2da36b2e11f2b74b5210f37190a070c5da
itsmeambro <alanbiju1998@gmail.com>
2025-12-08 23:16:10 +0530

    Kube cluster - bastion server removed

--- terraform/security.tf
+++ terraform/security.tf
@@ -31,14 +31,14 @@ resource "aws_security_group" "k8s_sg" {
   }
 
   # Access from bastion server
-  ingress {
-    description     = "SSH from any"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    # security_groups = [aws_security_group.bastion_sg.id]
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description     = "SSH from any"
+  #   from_port       = 22
+  #   to_port         = 22
+  #   protocol        = "tcp"
+  #   # security_groups = [aws_security_group.bastion_sg.id]
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
 
   egress {
